cd5032efa85bb0d9d423d883d9c4eb0416adb686
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-12-19 17:25:28 +0530

    added dynamic-ami

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,19 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_key_pair" "CloudDeploy_key" {
   key_name   = "clouddeploy-key"
   public_key = file("${path.module}/clouddeploy-key.pub") 
@@ -30,7 +46,7 @@ resource "aws_security_group" "CloudDeploy_sg" {
 }
 
 resource "aws_instance" "CloudDeploy" {
-  ami             = "ami-0c1c30571d2dae953" # Ubuntu 24.04 LTS in eu-west-1
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.CloudDeploy_key.key_name
   vpc_security_group_ids = [aws_security_group.CloudDeploy_sg.id]
@@ -42,11 +58,10 @@ resource "aws_instance" "CloudDeploy" {
               sudo systemctl start docker
               sudo systemctl enable docker
               # Pull and run your app from Docker Hub
-              sudo docker run -d -p 3000:3000 amitabhdevops/clouddeploy
+              sudo docker run -d -p 3000:3000 amitabhdevops/modern-node-app
               EOF
 
   tags = {
     Name = "CloudDeploy"
   }
-}
 
